1c78986ae86a886c9766a2abd8bc84fd528e4c0f
kareem <120470069+kareem221215@users.noreply.github.com>
2024-12-16 15:32:01 +0300

    Add files via upload

---
+++ patika+/README.md
@@ -0,0 +1,2 @@
+# patika+
+this is a repository for all my ptika+ projects
---
+++ patika+/patika+/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace patika_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // name and surname
+            Console.WriteLine("karem alsayed sleman ");
+            // why did i join  patika+?
+            Console.WriteLine("Patika'ya projeler yapmak, yeni insanlarla tanışmak ve en önemlisi simüle edilmiş gerçek bir yaşam dünyasında eğlenmek için katıldım");
+
+            Console.ReadLine();
+        }
+    }
+}
