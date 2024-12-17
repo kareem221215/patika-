ebbc4834afbe54c63da59cdbcc0628bdf2edcc2c
kareem <120470069+kareem221215@users.noreply.github.com>
2024-12-17 12:42:19 +0300

    Add files via upload

---
+++ if else/if else/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace if_else
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("pick a number");
+            long num = Convert.ToInt64(Console.ReadLine());
+            if (num > 10)
+            {
+                Console.WriteLine("number is greater than 10");
+            }
+            else if (num == 10)
+            {
+                Console.WriteLine("number is equal to 10");
+            }
+            else
+            {
+                Console.WriteLine("number is greater than 10");
+            }
+
+            if(num % 2 == 0)
+            {
+                Console.WriteLine("numebr is even");
+            }
+
+            else
+            {
+                Console.WriteLine("number is odd");
+            }
+            Console.ReadLine();
+        }
+    }
+}
